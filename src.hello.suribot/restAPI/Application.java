8da1ff7e47f2d752f376b14454b7def7a051be36
JulienM <julien.margarido@gmail.com>
2016-11-05 16:20:57 +0100

    add annotation main class, delete PORT in the code

--- src.hello.suribot/restAPI/Application.java
+++ src.hello.suribot/restAPI/Application.java
@@ -1,14 +1,20 @@
 package restAPI;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
+@EnableAutoConfiguration
+@ComponentScan
 @SpringBootApplication
 public class Application {
 	
     public static void main(String[] args) {
     	checkEnvVar();
-    	SpringApplication.run(Application.class, "--server.port=${static.port:12345}");
+    	SpringApplication.run(Application.class, "");
     }
     
     private static void checkEnvVar(){
