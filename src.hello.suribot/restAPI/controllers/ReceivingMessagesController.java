7dd5391681a9d04af9ec706e1052db82cdd0027c
JulienM <julien.margarido@gmail.com>
2016-11-05 16:30:52 +0100

    delete no-working code (send message function) for pull request

--- src.hello.suribot/restAPI/controllers/ReceivingMessagesController.java
+++ src.hello.suribot/restAPI/controllers/ReceivingMessagesController.java
@@ -7,23 +7,13 @@ import java.io.Reader;
 import javax.servlet.http.HttpServletRequest;
 
 import org.apache.http.HttpStatus;
-import org.bots4j.msbotframework.beans.ChannelAccount;
-import org.bots4j.msbotframework.beans.Message;
 import org.json.JSONException;
 import org.json.JSONObject;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import utils.msbotframework.HelloSuribotConnectorClient;
-
 @RestController
 public class ReceivingMessagesController {
-	
-	// TODO To change every hour : make a daemon to update the token, and put it in the cache
-	private static final String token = "";
-	
-	private static final String appid = System.getenv("APPID");
-	private static final String appsecret = System.getenv("APPSECRET");
 	
 	@RequestMapping(value ="/")
 	public int receivingMessage(HttpServletRequest request){
@@ -39,18 +29,6 @@ public class ReceivingMessagesController {
 		    }
 		    json = new JSONObject(sb.toString());
 		    printUserMessage(json);
-		    
-		    System.out.println("\n====sendResponse=====\n");
-		    HelloSuribotConnectorClient client = new HelloSuribotConnectorClient(appid, appsecret, token);
-		    Message message = new Message()
-		            .withFrom(from())
-		            .withTo(to())
-		            .withText("This is a test message from Hello Suribot")
-		            .withLanguage("en");
-
-		    printEntireJSONMessage(json);
-		    Message reply = client.Messages.sendMessage(message);
-		    System.out.println("Response : "+reply.getText());
 		    
 	    } catch (JSONException e){
 	    	System.out.println("No user message but a request has been received.");
@@ -71,21 +49,4 @@ public class ReceivingMessagesController {
 		System.out.println("User message : "+json.getString("text"));
 	}
 	
-	public static ChannelAccount to(){
-        return new ChannelAccount()
-                .withName("julien.margarido")
-                .withId("U2LJMA17F")
-//                .withAddress("julien.margarido")
-//                .withChannelId("slack")
-                .withIsBot(false);
-    }
-
-    public static ChannelAccount from(){
-        return new ChannelAccount()
-                .withName("lab_bot")
-                .withId("B2S5SJQ3Y")
-//                .withAddress("lab-bot")
-//                .withChannelId("slack")
-                .withIsBot(true);
-    }
 } 
