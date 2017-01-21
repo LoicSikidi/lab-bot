119bc2df00617243ef474b3e599c1407a2508d71
JulienMrgrd <julien.margarido@gmail.com>
2017-01-21 19:08:27 +0100

    test logs 8

--- src/hello/suribot/Application.java
+++ src/hello/suribot/Application.java
@@ -11,6 +11,8 @@ import hello.suribot.utils.EnvVar;
 public class Application {
 	
 	private static final Logger logger = LogManager.getLogger();
+	private static final Logger logger2 = LogManager.getRootLogger();
+	private static final Logger logger3 = LogManager.getFormatterLogger();
 	
     public static void main(String[] args) {
     	checkEnvVar();
@@ -20,7 +22,12 @@ public class Application {
     private static void checkEnvVar(){
 		
     	for(EnvVar var : EnvVar.values()){
-    		logger.error(var + " : "+ var.getValue());
+    		logger.info("1 "+var + " : "+ var.getValue());
+    		logger2.info("1 "+var + " :info2 "+ var.getValue());
+    		logger3.info("1 "+var + " :info3 "+ var.getValue());
+    		logger.error("1 "+var + " :1 "+ var.getValue());
+    		logger2.error("1 "+var + " :2 "+ var.getValue());
+    		logger3.error("1 "+var + " :3 "+ var.getValue());
     		if(var.getValue()==null){
     			logger.error("Check your environment var "+var.toString()+"... ");
     			System.exit(0);
