de2cfb57ca5a3f8e5d3de7da8ae8c21151699515
JulienMrgrd <julien.margarido@gmail.com>
2017-01-22 19:54:40 +0100

    renomme certaines variables

--- src/test/hello/suribot/communication/ai/AiControllerTest.java
+++ src/test/hello/suribot/communication/ai/AiControllerTest.java
@@ -1,9 +1,6 @@
 package test.hello.suribot.communication.ai;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotEquals;
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
 
 import org.json.JSONObject;
 import org.junit.Test;
