f7e5cef516853a823fb22d9ffcd285d9b71dd737
JulienMrgrd <julien.margarido@gmail.com>
2017-01-25 14:37:29 +0100

    Retour du test du analyze

--- src/test/hello/suribot/analyze/contracts/ContractAnalyzerTest.java
+++ src/test/hello/suribot/analyze/contracts/ContractAnalyzerTest.java
@@ -3,12 +3,23 @@ package test.hello.suribot.analyze.contracts;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNull;
 
+import org.json.JSONObject;
 import org.junit.Test;
 
+import hello.suribot.analyze.IntentsAnalyzer;
 import hello.suribot.analyze.contracts.ContractAnalyzer;
 
 public class ContractAnalyzerTest {
 	
+	@Test
+	public void analyzeTest(){
+		ContractAnalyzer analyzer = new ContractAnalyzer();
+		assertFalse(analyzer.analyze(null, null).optBoolean(IntentsAnalyzer.SUCCESS, false));
+		
+		analyzer = new ContractAnalyzer();
+		assertFalse(analyzer.analyze(new JSONObject(), null).optBoolean(IntentsAnalyzer.SUCCESS, false));
+	}
+
 	@Test
 	public void getCalledMethodTest() {
 		ContractAnalyzer analyzer = new ContractAnalyzer();
